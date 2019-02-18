84fdca1e962d81a53b3d9d738fe7df9f89ddfc55
Webfreak <info@webfreak.si>
2019-02-18 17:54:22 +0100

    Initial commit

---
+++ app/src/main/java/webfreak/si/remotecast/MainActivity.kt
@@ -0,0 +1,12 @@
+package webfreak.si.remotecast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
